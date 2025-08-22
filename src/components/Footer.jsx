00ed9a26de754c5fd2d0786add67ba330168b14f
anishasingx <anishasingh566@gmail.com>
2025-08-22 14:07:57 +0530

    Updated portfolio with new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { FaLinkedin, FaWhatsapp, FaTelegram } from "react-icons/fa";
+import { FaLinkedinIn, FaWhatsapp, FaTelegram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 const Footer = () => {
   return (
     <div className='row bg-primary py-3 px-5'>
         <div className="col-6 d-flex ">
-            <div className="mx-2 bg-white"><FaLinkedin size={20} color="#0A66C2" /></div>
+            <div className="mx-2 bg-white"><FaLinkedinIn size={20} color="#0A66C2" /></div>
             <div className="mx-2 bg-white"><FaWhatsapp size={20} color="#25D366" /></div>
             <div className="mx-2 bg-white"><SiGmail size={20} color="#EA4335" /></div>
             <div className="mx-2 bg-white"><FaTelegram size={20} color="#0088cc" /></div>
