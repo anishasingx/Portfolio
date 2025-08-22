dba0e7c1c26e4ee6469a7a31ec5686246dd90028
anishasingx <anishasingh566@gmail.com>
2025-08-22 14:22:26 +0530

    Updated portfolio with new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,17 @@ import { FaLinkedinIn, FaWhatsapp, FaTelegram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 const Footer = () => {
   return (
-    <div className='row bg-primary py-3 px-5'>
-        <div className="col-6 d-flex ">
-            <div className="mx-2 bg-white"><FaLinkedinIn size={20} color="#0A66C2" /></div>
-            <div className="mx-2 bg-white"><FaWhatsapp size={20} color="#25D366" /></div>
-            <div className="mx-2 bg-white"><SiGmail size={20} color="#EA4335" /></div>
-            <div className="mx-2 bg-white"><FaTelegram size={20} color="#0088cc" /></div>
+     <div className="fw-bold my-3 bg-primary py-3 px-5">
+        <div className="row justify-content-evenly">
+             <div className="col-6 d-flex">
+                <div className="mx-3 px-1  bg-white animate"><FaLinkedinIn size={15} color="#0A66C2" /></div>
+                <div className="mx-3 px-1  bg-white animate"><FaWhatsapp size={15} color="#25D366" /></div>
+                <div className="mx-3 px-1  bg-white animate"><SiGmail size={15} color="#E43315" /></div>
+                <div className="mx-3 px-1  bg-white animate"><FaTelegram size={15} color="#0088cc" /></div>
+            </div>
+            <div className="col-6 text-end text-white">Created By : Anisha Kumari</div>
         </div>
-        <div className="col-6 text-white text-end fw-bold">
-            Created By : Anisha Kumari
-        </div>
-        
-    </div>
+     </div>
   )
 }
 
