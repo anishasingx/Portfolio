68e0a7d1747c24facf8ecd5b07e66136c551fb7d
anishasingx <anishasingh566@gmail.com>
2025-08-22 14:26:56 +0530

    Updated portfolio with new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,43 @@
-import { FaLinkedinIn, FaWhatsapp, FaTelegram } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
-const Footer = () => {
+import { FaMusic, FaCamera, FaLaptopCode, FaPlane, FaFilm } from 'react-icons/fa';
+import { GiTennisRacket } from 'react-icons/gi';
+
+const Interest = () => {
   return (
-     <div className="fw-bold my-3 bg-primary py-3 px-5">
-        <div className="row justify-content-evenly">
-             <div className="col-6 d-flex">
-                <div className="mx-3 px-1  bg-white animate"><FaLinkedinIn size={15} color="#0A66C2" /></div>
-                <div className="mx-3 px-1  bg-white animate"><FaWhatsapp size={15} color="#25D366" /></div>
-                <div className="mx-3 px-1  bg-white animate"><SiGmail size={15} color="#E43315" /></div>
-                <div className="mx-3 px-1  bg-white animate"><FaTelegram size={15} color="#0088cc" /></div>
-            </div>
-            <div className="col-6 text-end text-white">Created By : Anisha Kumari</div>
+    <div>
+        <div className="display-6 fw-bold my-3 bg-danger text-white p-2">🎨 My Hobbies / Interest</div>
+        <div className="row text-center text-primary justify-content-center" >
+          <div>
+            <p className='text-justify-custom fs-5 text-center text-dark'>
+              First of all, I love music, especially country music, as it always keeps me relaxed and motivated. I have a great interest in photography, capturing special moments and exploring creativity through pictures. Programming is one of my core passions, and I enjoy building and experimenting with new projects to sharpen my skills. Playing badminton is another activity I love, as it helps me stay active and energetic. Traveling to new places gives me fresh experiences and perspectives, while watching movies in my free time helps me relax and enjoy good stories. Altogether, these interests make me a balanced and enthusiastic person, always eager to learn and explore more.
+            </p>
+          </div>
+          <div className="col-3 p-4">
+              <FaMusic  className='fs-1 mb-2'/>
+              <p>Music</p>
+          </div>
+          <div className="col-3 p-4">
+              <FaCamera className='fs-1 mb-2'/>
+              <p>Photo</p>
+          </div>
+          <div className="col-3 p-4">
+              <FaLaptopCode className='fs-1 mb-2'/>
+              <p>Programming</p>
+          </div>
+          <div className="col-3 p-4">
+              <FaPlane className='fs-1 mb-2'/>
+              <p>Traveling</p>
+          </div>
+          <div className="col-3 p-4">
+              <FaFilm className='fs-1 mb-2'/>
+              <p>Movies</p>
+          </div>
+          <div className="col-3 p-4">
+              <GiTennisRacket className='fs-1 mb-2'/>
+              <p>Bedminton</p>
+          </div>
         </div>
-     </div>
+    </div>
   )
 }
 
-export default Footer
+export default Interest
